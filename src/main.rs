bfb2986b63a5494051d5da56b7bfd57f869df0d8
Lakret <rakuretu@gmail.com>
2023-03-12 20:42:49 +0100

    input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use wasm_bindgen::prelude::*;
+use web_sys::HtmlInputElement;
 use yew::prelude::*;
 
 #[function_component]
@@ -11,8 +13,24 @@ fn App() -> Html {
         }
     };
 
+    let cell_val = use_state(|| String::new());
+    let onkeyup = {
+        let cell_val = cell_val.clone();
+        move |ev: KeyboardEvent| {
+            let input: HtmlInputElement = ev.target().unwrap().dyn_into().unwrap();
+            cell_val.set(input.value());
+        }
+    };
+
     html! {
         <div class="mx-auto container py-10 text-white text-xl grow-1">
+            <input type="text"
+                value={ (*cell_val).clone() }
+                onkeyup={onkeyup}
+                class="text-black" />
+            <p>{ (*cell_val).clone() }</p>
+
+            <br/>
             <button class="rounded-md cursor-pointer" {onclick}>{ "+1" }</button>
             <p>{ *counter }</p>
         </div>
