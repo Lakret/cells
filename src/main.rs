8ec65ac7d7a627f121336b83533582e772b9b5a4
Lakret <rakuretu@gmail.com>
2023-03-12 20:59:40 +0100

    a bunch of inputs

--- src/main.rs
+++ src/main.rs
@@ -24,10 +24,17 @@ fn App() -> Html {
 
     html! {
         <div class="mx-auto container py-10 text-white text-xl grow-1">
-            <input type="text"
-                value={ (*cell_val).clone() }
-                onkeyup={onkeyup}
-                class="text-black" />
+            <div class="flex flex-row flex-wrap">
+            {
+                (1..=10).map(|_input_idx| html! {
+                    <input type="text"
+                        value={ (*cell_val).clone() }
+                        onkeyup={onkeyup.clone()}
+                        class="bg-indigo-800 outline-none px-2 py-0.5" />
+                }).collect::<Html>()
+            }
+            </div>
+
             <p>{ (*cell_val).clone() }</p>
 
             <br/>
