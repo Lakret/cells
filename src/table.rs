c905740a3880dca2d84f0daaebacf2f253475b88
lakret <Rakuretu@gmail.com>
2023-04-20 19:27:29 +0200

    keyboard input for cells

--- src/table.rs
+++ src/table.rs
@@ -138,104 +138,109 @@ impl Component for Table {
           />
         </div>
 
-          <div class="overflow-scroll snap-y snap-mandatory pb-4">
-            <table class="table table-fixed">
-              <thead>
-                <tr class="snap-start">
-                  <th class="sticky top-0 left-0 snap-start pl-6 pr-4 z-40 w-full bg-indigo-900">
-                  </th>
-                  {
-                    // col id headers
-                    ('A'..='Z').map(move |col| {
-                      let header_style =
-                          match self.focused_cell {
-                              Some(CellId{ col: focused_col, .. }) if focused_col == col =>
-                                  "text-neutral-300 hover:text-neutral-200",
-                              _ => "text-neutral-400 hover:text-neutral-300",
-                          };
-
-                      html! {
-                        <th id={ format!("header-col-{col}") }
-                          class={classes!(vec![
-                              "z-30 sticky top-0 snap-start bg-clip-padding bg-indigo-900",
-                              "text-center",
-                              header_style
-                          ])}>
-                          { col }
-                        </th>
-                      }
-                    }).collect::<Html>()
-                  }
-                </tr>
-              </thead>
-              <tbody>
+        <div class="overflow-scroll snap-y snap-mandatory pb-4">
+          <table class="table table-fixed">
+            <thead>
+              <tr class="snap-start">
+                <th class="sticky top-0 left-0 snap-start pl-6 pr-4 z-40 w-full bg-indigo-900">
+                </th>
                 {
-                  (1..=50).map(move |row| {
+                  // col id headers
+                  ('A'..='Z').map(move |col| {
+                    let header_style =
+                        match self.focused_cell {
+                            Some(CellId{ col: focused_col, .. }) if focused_col == col =>
+                                "text-neutral-300 hover:text-neutral-200",
+                            _ => "text-neutral-400 hover:text-neutral-300",
+                        };
+
                     html! {
-                      <tr>
-                      {
-                        ('@'..='Z').map(move |col| {
-                          // row id header
-                          if col == '@' {
-                            let header_style =
-                              match self.focused_cell {
-                                Some(CellId{ row: focused_row, .. }) if focused_row == row =>
-                                  "text-neutral-300 hover:text-neutral-200",
-                                _ => "text-neutral-400 hover:text-neutral-300",
-                              };
-
-                            html! {
-                              <th id={ format!("header-row-{row}") }
-                                class={
-                                classes!(vec![
-                                    "z-[35] sticky left-0 snap-start pl-6 pr-4 bg-indigo-900 text-right",
-                                    header_style
-                                ])
-                              }>
-                                  { row }
-                              </th>
-                            }
-                          } else {
-                            let cell_id = CellId { col, row };
-                            html! {
-                              <Cell
-                                {cell_id}
-                                input={self.inputs.get(&cell_id).map(|x| x.clone())}
-                                expr={self.exprs.get(&cell_id).map(|x| x.clone())}
-                                computed={self.computed.get(&cell_id).map(|x| x.clone())}
-                                onfocus={
-                                  ctx.link().callback(move |ev: FocusEvent| {
-                                    let input: HtmlInputElement = ev.target().unwrap().dyn_into().unwrap();
-                                    let value = input.value();
-
-                                    Msg::CellFocused { cell_id, value }
-                                  })
-                                }
-                                onfocusout={
-                                  ctx.link().callback(move |ev: FocusEvent| {
-                                    Msg::CellLostFocus { cell_id }
-                                  })
-                                }
-                                oninput={
-                                  ctx.link().callback(move |ev: InputEvent| {
-                                    let input: HtmlInputElement = ev.target().unwrap().dyn_into().unwrap();
-                                    let new_value = input.value();
-
-                                    Msg::CellChanged { cell_id, new_value }
-                                  })
-                                }
-                              />
-                            }
-                          }
-                        }).collect::<Html>()
-                      }
-                      </tr>
+                      <th id={ format!("header-col-{col}") }
+                        class={classes!(vec![
+                            "z-30 sticky top-0 snap-start bg-clip-padding bg-indigo-900",
+                            "text-center",
+                            header_style
+                        ])}>
+                        { col }
+                      </th>
                     }
                   }).collect::<Html>()
                 }
-              </tbody>
-            </table>
-          </div>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                (1..=50).map(move |row| {
+                  html! {
+                    <tr>
+                    {
+                      ('@'..='Z').map(move |col| {
+                        // row id header
+                        if col == '@' {
+                          let header_style =
+                            match self.focused_cell {
+                              Some(CellId{ row: focused_row, .. }) if focused_row == row =>
+                                "text-neutral-300 hover:text-neutral-200",
+                              _ => "text-neutral-400 hover:text-neutral-300",
+                            };
+
+                          html! {
+                            <th id={ format!("header-row-{row}") }
+                              class={
+                              classes!(vec![
+                                  "z-[35] sticky left-0 snap-start pl-6 pr-4 bg-indigo-900 text-right",
+                                  header_style
+                              ])
+                            }>
+                                { row }
+                            </th>
+                          }
+                        } else {
+                          let cell_id = CellId { col, row };
+                          html! {
+                            <Cell
+                              {cell_id}
+                              input={self.inputs.get(&cell_id).map(|x| x.clone())}
+                              expr={self.exprs.get(&cell_id).map(|x| x.clone())}
+                              computed={self.computed.get(&cell_id).map(|x| x.clone())}
+                              onfocus={
+                                ctx.link().callback(move |ev: FocusEvent| {
+                                  let input: HtmlInputElement = ev.target().unwrap().dyn_into().unwrap();
+                                  let value = input.value();
+
+                                  Msg::CellFocused { cell_id, value }
+                                })
+                              }
+                              onfocusout={
+                                ctx.link().callback(move |ev: FocusEvent| {
+                                  Msg::CellLostFocus { cell_id }
+                                })
+                              }
+                              oninput={
+                                ctx.link().callback(move |ev: InputEvent| {
+                                  let input: HtmlInputElement = ev.target().unwrap().dyn_into().unwrap();
+                                  let new_value = input.value();
+
+                                  Msg::CellChanged { cell_id, new_value }
+                                })
+                              }
+                              sendinput={
+                                ctx.link().callback(move |new_value: String| {
+                                  Msg::CellChanged { cell_id, new_value }
+                                })
+                              }
+                            />
+                          }
+                        }
+                      }).collect::<Html>()
+                    }
+                    </tr>
+                  }
+                }).collect::<Html>()
+              }
+            </tbody>
+          </table>
+        </div>
       </div>
     }
   }
@@ -300,6 +305,9 @@ pub struct CellProps {
   pub onfocus: Callback<FocusEvent>,
   pub onfocusout: Callback<FocusEvent>,
   pub oninput: Callback<InputEvent>,
+  // sending a custom string as if it was inputted into cell - useful for processing of keyboard input
+  // on a focused cell, for example
+  pub sendinput: Callback<String>,
 }
 
 /**
@@ -310,6 +318,14 @@ fn Cell(props: &CellProps) -> Html {
   let div_select_mode = use_state(|| false);
   let input_mode = use_state(|| false);
   let input_ref = use_node_ref();
+
+  let input_value = props.input.clone().unwrap_or_default();
+
+  // if `computed_value` is present, show it in the div cell, otherwise show `value`
+  let div_value = match props.computed {
+    Some(Expr::Num(n)) => n.to_string(),
+    _ => props.input.clone().unwrap_or_default(),
+  };
 
   let onclick = {
     let div_select_mode = div_select_mode.clone();
@@ -334,6 +350,25 @@ fn Cell(props: &CellProps) -> Html {
     })
   };
 
+  let onkeypress = {
+    let input_mode = input_mode.clone();
+    let input_ref = input_ref.clone();
+
+    let parent_sendinput = props.sendinput.clone();
+
+    Callback::from(move |ev: KeyboardEvent| {
+      input_mode.set(true);
+
+      input_ref
+        .cast::<HtmlInputElement>()
+        .expect("ref is not attached to an input")
+        .focus()
+        .expect("cannot focus");
+
+      parent_sendinput.emit(ev.key());
+    })
+  };
+
   let div_onfocusout = {
     let div_select_mode = div_select_mode.clone();
     let parent_onfocusout = props.onfocusout.clone();
@@ -354,14 +389,6 @@ fn Cell(props: &CellProps) -> Html {
     })
   };
 
-  // if `computed_value` is present, show it in the div cell, otherwise show `value`
-  let div_value = match props.computed {
-    Some(Expr::Num(n)) => n.to_string(),
-    _ => props.input.clone().unwrap_or_default(),
-  };
-
-  let input_value = props.input.clone().unwrap_or_default();
-
   // note that the div gets a tabindex to allow focus & keyboard events;
   // `input_ref` is used to focus the input
   html! {
@@ -372,7 +399,7 @@ fn Cell(props: &CellProps) -> Html {
           id={ props.cell_id.to_string() }
           type="text"
           class={classes!(vec![
-            "px-2 py-0.5 w-[10rem] h-[2.125rem] outline-none text-right snap-start",
+            "px-2 py-0.5 w-[16rem] h-[2.125rem] outline-none text-right snap-start",
             "border-collapse border-[1px] border-indigo-900 bg-indigo-800 font-mono",
             if *input_mode { "z-10" } else { "z-0 select-none" }
           ])}
@@ -386,13 +413,14 @@ fn Cell(props: &CellProps) -> Html {
           id={ props.cell_id.to_string() }
           tabindex="0"
           class={classes!(vec![
-            "flex px-2 py-0.5 w-[10rem] -ml-[10rem] h-[2.125rem]",
+            "flex px-2 py-0.5 w-[16rem] -ml-[16rem] h-[2.125rem]",
             "border-[1px] border-indigo-900",
             if *input_mode { "z-0" } else { "z-10" },
             if *div_select_mode { "bg-indigo-700" } else { "bg-indigo-800" },
           ])}
           {onclick}
           {ondblclick}
+          {onkeypress}
           onfocusout={ div_onfocusout }
         >
           <span class="grow text-right select-none font-mono">{ div_value }</span>
